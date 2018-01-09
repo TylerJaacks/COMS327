1219917298d6572fa0a34d441ed5a0a9325aca68
Tyler Jaacks <tylerjaacks@gmail.com>
2018-01-09 17:07:01 -0600

    Fixed somethings.

--- Homework1/main.c
+++ Homework1/main.c
@@ -1,39 +1,13 @@
 #include <stdio.h>
 
-/* https://stackoverflow.com/questions/1921539/using-boolean-values-in-c Booleans in C. */
-typedef int bool;
-#define true 1
-#define false 0
-
-#define a 9 
-#define b 10
-#define c 11
-#define d 12
-#define e 13
-#define f 14
-#define g 15
-#define h 16
-
-int queens_position[4][4] = {{0,0}};
-
 int main(int argc, char* argv)
 {
-	calculate();
+	int queenCount = 0;
 
 	return 0;
 }
 
-bool calculate() 
+void calculate(int count)
 {
-	for (int i = 0; i < 10; i++) 
-	{
-		if (queens_position[i][i] == 0)
-		{
 
-		}
-
-		return true;
-	}
-
-	return false;
 }
