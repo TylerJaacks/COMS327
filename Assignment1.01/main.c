be02a176de15de8e8620df43d8191365da44946d
Tyler Jaacks <tylerjaacks@gmail.com>
2018-01-18 19:55:22 -0600

    Got room placement working.

--- Assignment1.01/main.c
+++ Assignment1.01/main.c
@@ -1,22 +1,82 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define rock ' '
-#define floor '.'
-#define corridor '#'
+typedef int bool;
+#define true 1
+#define false 0
 
-int height = 21;
-int width = 80;
+typedef struct
+{
+	int x_pos;
+	int y_pos;
+	int x_size;
+	int y_size;
+} room;
 
-int number_of_rooms = 5;
+typedef struct
+{
+	room rooms[5];
+} dungeon;
+
+char screen[21][80];
 
 int main(int argc, char *argv)
 {
-	create_dungeon(height, width, number_of_rooms);
+	srand(time(NULL));
+
+	int rand_x_pos = rand() % 10 + 1;
+	int rand_y_pos = rand() % 10 + 1;
+	int rand_x_size = rand() % 10 + 1;
+	int rand_y_size = rand() % 10 + 1;
+
+	init_array();
+
+	place_room(rand_x_pos, rand_y_pos, 5, 5);
+
+	print_array();
 
 	return 0;
 }
 
-void create_dungeon(int width, int height, int n)
+void place_room(int x_pos, int y_pos, int x_size, int y_size)
+{
+	printf("x_pos: %d y_pos: %d x_size: %d y_size: %d \n", x_pos, y_pos, x_size, y_size);
+
+	for (int i = x_pos; i < x_pos + x_size; i++)
+	{
+		for (int j = y_pos; j < y_pos + y_size; j++)
+		{
+			screen[i][j] = '.';
+		}
+	}
+}
+
+bool does_conflict()
 {
 
+}
+
+void init_array()
+{
+	for (int i = 0; i < 21; i++)
+	{
+		for (int j = 0; j < 80; j++)
+		{
+			screen[i][j] = ' ';
+		}
+	}
+}
+
+void print_array()
+{
+	for (int i = 0; i < 21; i++)
+	{
+		for (int j = 0; j < 80; j++)
+		{
+			printf("%c", screen[i][j]);
+		}
+
+		printf("\n");
+	}
 }
